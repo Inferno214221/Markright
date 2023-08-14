88ce509efe76049d921d32325147028c22c787d3
Inferno214221 <inferno214221@gmail.com>
2023-08-14 16:53:48 +0930

    Added Empty Electron Project

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
+const path = require("path")
+const fs = require("fs-extra");
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 1120,
+        height: 630,
+        minWidth: 810,
+        minHeight: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        },
+        autoHideMenuBar: true
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
